adc2029f7c394953f0d43f3869964b4eaec761b6
Tom Käppler <45234980+TKSpectro@users.noreply.github.com>
2021-04-15 16:40:52 +0200

    finish series2 mostly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "./src/series1.h"
+#include "./src/series2.h"
 
 void series1(int a, int b) {
     std::cout << "--- Series 1 ---" << std::endl;
@@ -15,8 +16,30 @@ void series1(int a, int b) {
     std::cout << "--- Series 1 END ---" << std::endl;
 }
 
+void series2() {
+    std::cout << "--- Series 2 ---" << std::endl;
+
+    std::string plaintext = "ANGEWANDTEINFORMATIK";
+    std::cout << "Klartext: " << plaintext << std::endl;
+    std::string key = "KEY";
+    std::cout << "Schluessel: " << key << std::endl;
+
+    std::string encodedText = encodeVigenere(plaintext, key);
+    std::cout << "Geheimtext: " << encodedText << std::endl;
+
+    std::cout << "Frequency Check:" << std::endl;
+
+    int *frequencyArray = new int[26];
+    frequencyArray = countLetterFrequency("JA MOIN WAS GEHT", frequencyArray);
+    outputLetterFrequencies(frequencyArray, true);
+
+    std::cout << "--- Series 2 END ---" << std::endl;
+}
+
+
 int main() {
-    series1(93, 42);
+    // series1(93, 42);
+    series2();
 
     return 0;
 }
